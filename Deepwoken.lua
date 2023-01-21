4582c687470c1c94ac7b2c46743aaeb14b9fe254
BillsTheGod <69477637+BillsTheGod@users.noreply.github.com>
2023-01-21 07:56:05 -0300

    Update Deepwoken.lua

--- Deepwoken.lua
+++ Deepwoken.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/BillsTheGod/V3RMILLION_PROJECTS/main/el%20jeleba's%20hub/Hub%20Loader.lua")))();
+loadstring(syn.request({Url="https://raw.githubusercontent.com/BillsTheGod/V3RMILLION_PROJECTS/main/el%20jeleba's%20hub/Hub%20Loader.lua"}).Body)();
