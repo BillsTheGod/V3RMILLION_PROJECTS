932ffe74d8427a85e3e08c77c16c03b5f8a5c089
BillsTheGod <69477637+BillsTheGod@users.noreply.github.com>
2023-01-21 07:33:02 -0300

    Update Hub Loader.lua

--- el jeleba's hub/Hub Loader.lua
+++ el jeleba's hub/Hub Loader.lua
@@ -2,9 +2,9 @@ if not syn then game:GetService("Players").LocalPlayer:Kick("Your exploit is not
 
 local GameIDs = {
 	[1359573625] = "Deepwoken",
-	[2132098792] = "Hollow Abyss"
+	[2132098792] = "Hollow%20Abyss"
 };
 
 if GameIDs[game.GameId] then
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/BillsTheGod/V3RMILLION_PROJECTS/main/el%20jeleba's%20hub/" .. string.gsub(GameIDs[game.GameId], " ", "%20") .. ".lua")))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/BillsTheGod/V3RMILLION_PROJECTS/main/el%20jeleba's%20hub/" .. GameIDs[game.GameId] .. ".lua")))()
 end
