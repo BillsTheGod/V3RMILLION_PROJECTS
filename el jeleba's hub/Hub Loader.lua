fe075614f5f895219d408b19b9c64d82373c2dc6
BillsTheGod <69477637+BillsTheGod@users.noreply.github.com>
2023-01-20 22:57:34 -0300

    Create Hub Loader.lua

---
+++ el jeleba's hub/Hub Loader.lua
@@ -0,0 +1,10 @@
+if not syn then game:GetService("Players").LocalPlayer:Kick("Your exploit is not supported !") end;
+
+local GameIDs = {
+	[1359573625] = "Deepwoken",
+	[2132098792] = "Hollow Abyss"
+};
+
+if GameIDs[game.GameId] then
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/BillsTheGod/V3RMILLION_PROJECTS/main/el%20jeleba's%20hub/" .. GameIDs[game.GameId] .. ".lua")))()
+end
